c44cc2fb6f8c4da40cdc280f7e09743214925cb5
ManaMauri <147702066+ManaMauri@users.noreply.github.com>
2023-10-13 20:57:03 +1300

    bloody hell

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Set the date we're counting down to
 const countDownDate = new Date("December 20, 2023 00:00:00").getTime();
 
-const face = document.getElementById('face');
-const audioPlayer = document.getElementById('audioPlayer');
-
 // Update the countdown every second
 const interval = setInterval(function() {
     const now = new Date().getTime();
@@ -21,11 +18,6 @@ const interval = setInterval(function() {
     document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
     document.getElementById("seconds").textContent = String(seconds).padStart(2, '0');
 
-    // Change face expression based on time left
-    if (days < 1) {
-        face.classList.add('excited');
-    }
-
     // If the countdown is finished, display a message
     if (distance < 0) {
         clearInterval(interval);
@@ -34,19 +26,13 @@ const interval = setInterval(function() {
 }, 1000);
 
 // Add event listeners for the smiley face interaction
+const face = document.getElementById('face');
+const audioPlayer = document.getElementById('audioPlayer');
+
 face.addEventListener('click', function() {
     if (audioPlayer.paused) {
         audioPlayer.play(); // Play the MP3
     } else {
         audioPlayer.pause(); // Pause the MP3
     }
-    face.classList.remove('surprised'); // Revert to the current state (either normal or excited)
-});
-
-face.addEventListener('mouseover', function() {
-    face.classList.add('surprised');
-});
-
-face.addEventListener('mouseout', function() {
-    face.classList.remove('surprised');
 });
