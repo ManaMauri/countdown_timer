364af22cc3beb33471c9b422ce334c7b657a1345
ManaMauri <147702066+ManaMauri@users.noreply.github.com>
2023-10-13 15:30:59 +1300

    Revision and Music

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const countdown = document.querySelector('.countdown');
-
 // Set the date we're counting down to
 const countDownDate = new Date("December 20, 2023 00:00:00").getTime();
 
@@ -20,20 +18,21 @@ const interval = setInterval(function() {
     document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
     document.getElementById("seconds").textContent = String(seconds).padStart(2, '0');
 
-    // Calculate the percentage of time remaining
-    const totalDuration = countDownDate - new Date("October 11, 2023 00:00:00").getTime();
-    const percentageRemaining = distance / totalDuration;
-
-    // Adjust the circle's size based on the percentage of time remaining
-    const circle = document.querySelector('.circle');
-    const maxSize = 300; // the initial size of the circle in pixels
-    const newSize = maxSize * Math.sqrt(percentageRemaining); // Using the square root for a more visually appealing effect
-    circle.style.width = `${newSize}px`;
-    circle.style.height = `${newSize}px`;
-
     // If the countdown is finished, display a message
     if (distance < 0) {
         clearInterval(interval);
-        countdown.innerHTML = "HOLIDAY TIME!";
+        document.querySelector('.countdown').innerHTML = "HOLIDAY TIME!";
     }
 }, 1000);
+
+// Add event listeners for the smiley face interaction
+const face = document.getElementById('face');
+const audioPlayer = document.getElementById('audioPlayer');
+
+face.addEventListener('click', function() {
+    if (audioPlayer.paused) {
+        audioPlayer.play(); // Play the MP3
+    } else {
+        audioPlayer.pause(); // Pause the MP3
+    }
+});
